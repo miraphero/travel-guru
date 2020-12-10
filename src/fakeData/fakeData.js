ade05e7c3e0f8080fe521aff650df1ab766feb44
miraphero <mirarahman949@gmail.com>
2020-12-10 16:24:59 +0600

    done

---
+++ src/fakeData/fakeData.js
@@ -0,0 +1,22 @@
+
+const travelData = [
+    {
+        "id":"0",
+        "TravelTitle":"COX'S BAZAR",
+        "TravelBody":"Cox's Bazar is a city, fishing port, tourism centre and district headquarters in southeastern Bangladesh. It is famous mostly for its long natural sandy beach, and it is infamous for the largest refugee camp in the world. It is located 150 km (93 mi) south of the divisional headquarter city of Chittagong.",
+        "img":"https://i.imgur.com/5Otlp9e.png",
+    },
+    {
+        "id":"1",
+        "TravelTitle":"SREEMANGAL",
+        "TravelBody":"Sreemangal is situated in Moulvibazar district in sylhet division. Sreemangal is an Upazila. It is famous for tea garden. Rain all time occurs here. Nature has adorned sreemangal with green tress. Its natural scenery is very charming..",
+        "img":"https://i.imgur.com/jqBytFb.png",
+    },
+    {
+        "id":"2",
+        "TravelTitle":"SUNDARBANS",
+        "TravelBody":"The Sundarbans is a mangrove area in the delta formed by the confluence of the Ganges, Brahmaputra and Meghna Rivers in the Bay of Bengal. It spans from the Hooghly River in India's state of West Bengal to the Baleswar River in Bangladesh. It comprises closed and open mangrove forests, agriculturally used land, mudflats and barren land, and is intersected by multiple tidal streams and channels.",
+        "img":"https://i.imgur.com/uGbTjWE.png",
+    },
+];
+export default travelData;
